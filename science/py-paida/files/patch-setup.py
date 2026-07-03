3a925af2b22cce52c07accfadc31c08b1d8e6a16
Hiroo Ono <hiroo.ono+freebsd@gmail.com>
2026-07-04 06:19:50 +0900

    science/py-paida: fix build with setuptools 81.0.0

---
+++ science/py-paida/files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2026-04-16 08:12:40 UTC
++++ setup.py
+@@ -49,7 +49,7 @@ setup (name = "paida",
+ 	'paida.tools']
+ 
+ setup (name = "paida",
+-       version = "3.2.1_2.10.1",
++       version = "3.2.1+2.10.1",
+        author = "Koji Kishimoto",
+        author_email = "korry@users.sourceforge.net",
+        url = "http://paida.sourceforge.net/",
