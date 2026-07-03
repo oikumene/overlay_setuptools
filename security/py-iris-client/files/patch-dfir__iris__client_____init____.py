68cdff7feac2aed8039dad978d23752533780137
Hiroo Ono <hiroo.ono+freebsd@gmail.com>
2026-07-04 05:38:05 +0900

    security/py-iris-client: fix build with setuptools 81.0.0

---
+++ security/py-iris-client/files/patch-dfir__iris__client_____init____.py
@@ -0,0 +1,22 @@
+--- dfir_iris_client/__init__.py.orig	2023-05-15 14:17:25 UTC
++++ dfir_iris_client/__init__.py
+@@ -1 +1,19 @@
++"""
++iris Client API Source Code
++contact@dfir-iris.org
+ 
++This program is free software; you can redistribute it and/or
++modify it under the terms of the GNU Lesser General Public
++License as published by the Free Software Foundation; either
++version 3 of the License, or (at your option) any later version.
++
++This program is distributed in the hope that it will be useful,
++but WITHOUT ANY WARRANTY; without even the implied warranty of
++MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
++Lesser General Public License for more details.
++
++You should have received a copy of the GNU Lesser General Public License
++along with this program; if not, write to the Free Software Foundation,
++Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
++"""
++__version__ = "2.0.1"
