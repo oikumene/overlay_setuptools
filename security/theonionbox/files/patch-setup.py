7a5849f566c8b8515c98df2396f1346f38412c96
Hiroo Ono <hiroo.ono+freebsd@gmail.com>
2026-07-04 08:04:32 +0900

    security/theonionbox: fix build with setuptools 81.0.0

---
+++ security/theonionbox/files/patch-setup.py
@@ -0,0 +1,44 @@
+--- setup.py.orig	2019-05-10 21:59:01 UTC
++++ setup.py
+@@ -295,19 +295,9 @@ data_files = [
+ }
+ 
+ data_files = [
+-    ('docs', ['docs/*.*']),
+-    ('', ['readme/README.html']),
+-    ('config', ['theonionbox/config/*.*']),
+-    ('service', []),
+-    ('service/FreeBSD', ['FreeBSD/theonionbox.sh']),
+-    ('service/init.d', ['init.d/theonionbox.sh']),
+-    ('service/systemd', ['systemd/theonionbox.service']),
+-    ('service/Docker', ['Docker/Dockerfile', 'Docker/theonionbox.cfg']),
+-    ('support', []),
+-    ('support/osxtemp', []),
+-    ('support/osxtemp/libsmc', ['support/osxtemp/libsmc/LICENSE', 'support/osxtemp/libsmc/Makefile']),
+-    ('support/osxtemp/libsmc/include', ['support/osxtemp/libsmc/include/smc.h']),
+-    ('support/osxtemp/libsmc/src', ['support/osxtemp/libsmc/src/smc.c']),
++    ('share/doc/theonionbox', ['docs/*.*']),
++    ('etc/theonionbox', ['theonionbox/config/*.*']),
++    ('etc/rc.d', ['FreeBSD/theonionbox']),
+ ]
+ # print(generate_data_files(data_files))
+ 
+@@ -376,15 +366,15 @@ setup(
+     },
+     install_requires=[
+         'psutil>=5.4.0',
+-        'apscheduler>=2.1.2, <3.*; python_version<"3.0"',
++        'apscheduler>=2.1.2, <3; python_version<"3.0"',
+         'apscheduler>=3.4; python_version>="3.0"',
+         'requests>=2.21',
+         'PySocks>=1.6.7',
+         'bottle>=0.12.13',
+-        'stem>=1.5.4, <=1.6',
++        'stem>=1.5.4',
+         'tzlocal>=1.5',
+         'futures>=3.2; python_version<"3.0"',
+-        'urllib3>=1.24.2, <1.25'    # '<1.25' due to requests 2.21 requirement
++        'urllib3>=1.21.1' # copied (except the exclusions) from requests 2.25.1
+     ],
+     long_description_content_type='text/x-rst; charset=UTF-8',
+     classifiers=[
